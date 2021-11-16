8e48e8252030d2ca29f84c9edd8a6c82f29922c6
PashaKulikov <paku.it.java@gmail.com>
2021-11-16 21:34:58 +0300

    init

--- src/Lesson4/HomeWork/RGD.java
+++ src/Lesson4/HomeWork/RGD.java
@@ -1,4 +1,11 @@
 package Lesson4.HomeWork;
 
 public class  RGD {
+    public static void main(String[] args) {
+    Train train1 = new Train(30,"Penza","passenger");
+    train1.go(50);
+    train1.setStop(true);
+    train1.viewedStatus();
+
+    }
 }
---
+++ src/Lesson4/HomeWork/Train.java
@@ -0,0 +1,35 @@
+package Lesson4.HomeWork;
+
+public class Train {
+    private  int speed;
+   private boolean isComing;
+   private  boolean isStanding;
+   private  int numbersOfWagons;
+   private String direction;
+   private String typeOfTransportation;
+
+
+    public Train(int numbersOfWagons, String direction, String typeOfTransportation) {
+
+        this.numbersOfWagons = numbersOfWagons;
+        this.direction = direction;
+        this.typeOfTransportation = typeOfTransportation;
+    }
+
+    public void go (int currentSpeed){
+        isComing = true;
+        speed = currentSpeed;
+        System.out.println("The train started to move"+ " his speed is "+ currentSpeed);
+    }
+
+    public void setStop(boolean stop ){
+        isStanding = stop;
+        System.out.println("The train stopped");
+    }
+    public void viewedStatus(){
+        System.out.println((isStanding == false)?"The train is coming ":"The train is stopped");
+    }
+
+
+
+}
